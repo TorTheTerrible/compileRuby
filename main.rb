0fccd6d9219f5197b6a75bfd91feab181878127d
suika <suika@debian.com>
2025-12-18 09:47:04 -0500

    modified:   main.rb

--- main.rb
+++ main.rb
@@ -6,6 +6,8 @@ $e = []
 $g = []
 
 $p
+$f = 0
+$mainPresent = false
 
 def match(char)
 	unless $e[$i]
@@ -24,6 +26,10 @@ def match(char)
 end
 
 def program
+	$g.append('.file "code.c"')
+	$g.append(".text")
+	$g.append(".globl main")
+	$g.append(".type main, @function")
 	function
 end
 
@@ -36,13 +42,47 @@ def bracket
 end
 
 def function
-	if match(:keyword)
+	#sanity checks
+	mainPresent = false
+	if $e[$i] == nil
+		return
+	end
+
+	if $e[$i].type == :keyword
+		match(:keyword)
+		if $p.value == "main"
+			mainPresent = true
+			$mainPresent = true
+		end		
+
 		match(:id)
+
+		#add function start
+		$g.append($p.value + ":")
+		$g.append(".LFB*:".gsub("*", $f.to_s))
+		$g.append(".cfi_startproc")
+		$g.append("pushq %rbp")
+		$g.append(".cfi_def_cfa_offset 16")
+		$g.append(".cfi_offset 6, -16")
+		$g.append("movq %rsp, %rbp")
+		$g.append(".cfi_def_cfa_register 6")
+
 		match(:paren)
 		match(:paren)
 		bracket
 
+		$g.append(".cfi_def_cfa 7, 8")
+		$g.append("ret")
+		$g.append(".cfi_endproc")
+
+		#add function end
+		$g.append(".LFE*:".gsub("*", $f.to_s))
+		$g.append(".size main, .-main")
+		$g.append('.ident "GCC: (Debian 14.2.0-19) 14.2.0"')
+		$g.append('.section .note.GNU-stack,"",@progbits')
+
 		puts "function parsed"
+		$f += 1
 		function
 	end
 end
@@ -54,9 +94,9 @@ end
 def returnValue
 	if match(:ret)
 		expresion
-		$g.append("mov eax, " + $p.value)
+		$g.append("movl $*, %eax".gsub("*", $f.to_s))
+		$g.append("popq %rbp")
 		match(:eol)
-		$g.append("ret")
 	end
 end
 
@@ -64,6 +104,7 @@ def expresion
 	match(:int)
 end
 
-$e = createTokens("code.txt")
+$e = createTokens("code.c")
 program
+
 puts $g
