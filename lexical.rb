22b2dd149c0a81232295c43a6debba117d1a83ed
TorTheTerrible <167561952+TorTheTerrible@users.noreply.github.com>
2026-01-28 09:09:35 -0500

    Update lexical.rb

--- lexical.rb
+++ lexical.rb
@@ -1,1 +1,51 @@
 
+contents
+step = 0
+
+def consumeChar()
+  temp = []
+  while true
+    if contents[i] != " "
+      temp.append(contents[i])
+      step += 1
+    else
+      step += 1
+      break
+    end
+  end
+  return temp
+end
+
+def lex()
+  while true
+    
+  end
+end
+
+def identifier()
+  if contents[step] = letter
+    ident
+  end
+end
+
+def keyword()
+  
+end
+
+def number()
+
+end
+
+def opperator()
+
+end
+
+def generateTokens()
+  tokens = []
+  
+  #generate the tokens
+  while loop
+    tokens.append("identifier")
+
+  end
+end
