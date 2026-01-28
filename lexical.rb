d64b2aada2bca977a8a6c67e286c111e68430be6
TorTheTerrible <167561952+TorTheTerrible@users.noreply.github.com>
2026-01-28 09:17:39 -0500

    Update lexical.rb

--- lexical.rb
+++ lexical.rb
@@ -4,8 +4,14 @@ step = 0
 
 def consumeChar()
   temp = []
+  
+  if contents[i] = "(",")"
+    temp.append(contents[i])
+    return temp
+  end
+  
   while true
-    if contents[i] != " "
+    if contents[i] != " ","(",")"
       temp.append(contents[i])
       step += 1
     else
@@ -18,7 +24,21 @@ end
 
 def lex()
   while true
+    temp = consumeChar()
     
+    if temp == "(" or if temp == ")"
+      tokens.append("()")
+    else
+
+    if temp == ";"
+      tokens.append(";")
+    else
+
+    if temp == "if"
+      tokenss.append("if")
+    else
+      tokens.append("id")
+    end  
   end
 end
 
